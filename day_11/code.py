bf5b35a2d021e10892c0ce92660d66cdf66abbe3
ufJmacca <nitro.jmacca@gmail.com>
2024-12-11 11:18:07 +0000

    ADD recursive approach with caching as multithreading approach did not work

--- day_11/code.py
+++ day_11/code.py
@@ -1,64 +1,34 @@
 import requests
 import os
 from dotenv import load_dotenv
-import concurrent.futures
-from collections import deque
+from functools import cache
 
 def prep_input(input: str) -> list[list[str]]:
     return [int(line) for line in input.strip().split()]
 
 
-def check_and_append(value: int) -> list[int]:
+@cache
+def check_value(value: int, steps: int) -> int:
+    if steps == 0:
+        return 1
     if value == 0:
-        return [1]
+        return check_value(1, steps -1)
     if len(str(value)) % 2 == 0:
         mid = len(str(value)) // 2
-        return [int(str(value)[:mid]), int(str(value)[mid:])]
+        return check_value(int(str(value)[:mid]), steps -1) + check_value(int(str(value)[mid:]), steps -1)
     else:
-        return [value*2024]
-
-def blink(input: list[int]) -> list[int]:
-    with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
-        queue = deque(executor.submit(check_and_append, value) for value in input)
-        next_list = []
-
-        while queue:
-            future = queue.popleft()
-            try:
-                result = future.result()
-                next_list.extend(result)
-            except Exception as e:
-                print(f"Error processing: {e}")
-    
-    return next_list
-
-# def blink(input: list[int]) -> list[int]:
-#     next_list = []
-#     for i in range(len(input)):
-#         result = check_and_append(input[i])
-#         next_list.extend(result)
-#     return next_list
+        return check_value(value*2024, steps -1)
 
 def advent_1(input: str, blinks: int) -> int:
     input = prep_input(input)
-    # print(input)
-
-    for _ in range(blinks):
-        input = blink(input)
-        # print(input)
     
-    return len(input)
+    return sum(check_value(value, blinks) for value in input)
 
 def advent_2(input: str, blinks: int) -> int:
     input = prep_input(input)
-    # print(input)
-
-    for i in range(blinks):
-        print(f"blink number {i}")
-        input = blink(input)
-        # print(input)
     
-    return len(input)
+    return sum(check_value(value, blinks) for value in input)
+
 if __name__ == "__main__":
     load_dotenv()
 
